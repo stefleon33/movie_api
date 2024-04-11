682413ecf4574b5716d9f54c32aaa238c435de47
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-04-11 13:38:16 -0400

    Top 10 movie info

---
+++ index.js
@@ -0,0 +1,62 @@
+let topMovies = [
+  {
+    title: 'The Shawshank Redemption',
+    releaseDate: '1994',
+    rating: 'R',
+    runTime: '2h 22m'
+  },
+  {
+    title: 'The Godfather',
+    releaseDate: '1972',
+    rating: 'R',
+    runTime: '2h 55m'
+  },
+  {
+    title: 'The Dark Knight',
+    releaseDate: '2008',
+    rating: 'PG-13',
+    runTime: '2h 32m'
+  },
+  {
+    title: 'The Godfather Part II',
+    releaseDate: '1974',
+    rating: 'R',
+    runTime: '3h 22m'
+  },
+  {
+    title: '12 Angry Men',
+    releaseDate: '1957',
+    rating: 'Approved',
+    runTime: '1h 36m'
+  },
+  {
+    title: 'Schindler\'s List',
+    releaseDate: '1993',
+    rating: 'R',
+    runTime: '3h 15m'
+  },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+    releaseDate: '2003',
+    rating: 'PG-13',
+    runTime: '3h 21m'
+  },
+  {
+    title: 'Pulp Fiction',
+    releaseDate: '1994',
+    rating: 'R',
+    runTime: '2h 34m'
+  },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    releaseDate: '2001',
+    rating: 'PG-13',
+    runTime: '2h 58m'
+  },
+  {
+    title: 'The Good, the Bad and the Ugly',
+    releaseDate: '1966',
+    rating: 'R',
+    runTime: '2h 58m'
+  },
+];
