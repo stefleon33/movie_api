efe2173247f930ef8e81136217bbd2ea6025653e
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-04-16 21:37:36 -0400

    Updated required info for The Good, the Bad and the Ugly

--- index.js
+++ index.js
@@ -197,10 +197,24 @@ let users =[
     "Featured":false
   },
   {
-    title: 'The Good, the Bad and the Ugly',
-    releaseDate: '1966',
-    rating: 'R',
-    runTime: '2h 58m'
+    "Title": "The Good, the Bad and the Ugly",
+    "Description":"A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.",
+    //"Stars":["Clint Eastwood","Eli Wallach", "Lee Van Cleef"],
+    "Release date": "1966",
+    "Rating": "R",
+    "Run time": "2h 58m",
+    "Genre":{
+      "Name":"Adventure", //"Western"
+      "Description":"Adventure films take audiences on exhilarating journeys to exotic locales, uncharted territories, and fantastical worlds. They often feature daring protagonists embarking on quests, expeditions, or treasure hunts filled with danger, excitement, and discovery. Adventure films blend elements of action, exploration, and escapism, offering thrills and escapades that capture the imagination. Whether set in the jungles of South America, the high seas, or distant galaxies, adventure films invite viewers to join in the thrill of the unknown and the joy of exploration."
+    },
+    "Director":{
+      "Name":"Sergio Leone",
+      "Bio":"Sergio Leone was virtually born into the cinema - he was the son of Roberto Roberti (A.K.A. Vincenzo Leone), one of Italy's cinema pioneers, and actress Bice Valerian. Leone entered films in his late teens, working as an assistant director to both Italian directors and U.S. directors working in Italy (usually making Biblical and Roman epics, much in vogue at the time). Towards the end of the 1950s he started writing screenplays, and began directing after taking over The Last Days of Pompeii (1959) in mid-shoot after its original director fell ill. His first solo feature, The Colossus of Rhodes (1961), was a routine Roman epic, but his second feature, A Fistful of Dollars (1964), a shameless remake of Akira Kurosawa's Yojimbo (1961), caused a revolution. It was the first Spaghetti Western, and shot T.V. cowboy Clint Eastwood to stardom (Leone wanted Henry Fonda or Charles Bronson but couldn't afford them). The two sequels, For a Few Dollars More (1965) and The Good, the Bad and the Ugly (1966), were shot on much higher budgets and were even more successful, though his masterpiece, Once Upon a Time in the West (1968), in which Leone finally worked with Fonda and Bronson, was mutilated by Paramount Pictures and flopped at the U.S. box office. He directed Duck, You Sucker! (1971) reluctantly (as producer he hired Peter Bogdanovich to direct but he left before shooting began), and turned down offers to direct The Godfather (1972) in favor of his dream project, which became Once Upon a Time in America (1984). He died in 1989 after preparing an even more expensive Soviet co-production on the World War II siege of Leningrad.",
+      "Birth":"January 3, 1929",
+      "Death":"April 30, 1989",
+    },
+    "ImageURL":"",
+    "Featured":false
   },
 ];
 
