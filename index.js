ba5ecf9304e9b048a37cdb5052bbed5c4ae9dd8d
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-05-06 22:18:58 -0400

    Updated user delete function

--- index.js
+++ index.js
@@ -282,19 +282,20 @@ app.delete('/users/:id/:movieTitle', (req, res) =>{
 })
 
 //DELETE Allow existing users to deregister (showing only a text that a user email has been removed)
-app.delete('/users/:id', (req, res) =>{
-  const { id } = req.params;
-  const updatedUser = req.body;
-  
-  let user = users.find( user => user.id == id)
-
-  if (user) {
-    users = users.filter (user => user.id != id);
-    res.status(200).send(`User ${id} has been deleted.`);
-  } else { 
-    res.status(400).send('No such user.')
-  }
-})
+app.delete('/users/:Username', async (req, res) => {
+  await Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 //READ. Return a list of ALL movies to the user
 app.get('/movies', async (req, res) => {
