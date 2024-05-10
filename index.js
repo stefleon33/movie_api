32ddbe93fa0b0555dbcee8be6618c88bb0ebbc49
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-05-10 16:42:49 -0400

    Code for hashed password

--- index.js
+++ index.js
@@ -33,15 +33,17 @@ require('./passport');
   Birthday: Date
 }*/
 app.post('/users', async (req, res) => {
-  await Users.findOne({ Username: req.body.Username})
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOne({ Username: req.body.Username}) // Search to see if a user with the requested username already exists
   .then((user) => {
     if (user) {
+    //If the user is found, send a response that it already exists
       return res.status(400).send(req.body.Username + 'already exists');
     } else {
       Users
         .create({
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday
         })
