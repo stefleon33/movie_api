7bdd1ba0a24a8bec3e069d68b8de078be22d7a30
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-05-14 19:25:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 
 const express = require('express'),
