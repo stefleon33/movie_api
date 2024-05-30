669e2b08e5842279ef5efe8199f9a9f5b9efa6c1
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-05-30 10:39:51 -0400

    removed unnecessary code

--- index.js
+++ index.js
@@ -17,18 +17,6 @@ const express = require('express'),
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let auth = require('./auth')(app);
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
-
-app.use(cors({
- origin: (origin, callback) => {
-   if(!origin) return callback(null, true);
-   if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-     let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-     return callback(new Error(message ), false);
-   }
-   return callback(null, true);
- }
-}));
 
 const passport = require('passport');
 require('./passport');
