018876f67f0b91772be00255e72c17cf820b979d
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-11-13 12:30:14 -0500

    JSdoc updates

--- models.js
+++ models.js
@@ -1,14 +1,14 @@
+/**
+ * @module models
+ * @description This module defines the data models used in the application, including `Movie` and `User`.
+ * It also includes methods for hashing and validating user passwords.
+ */
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 /**
  * @schema Movie
  * @description Mongoose schema for a Movie. Contains fields such as title, description, genre, director, and more.
- */
-
-
-/**
- * @typedef {Object} Movie
  * @property {string} Title - The title of the movie.
  * @property {string} Description - A description of the movie plot.
  * @property {Genre} Genre - An object containing genre information.
@@ -16,16 +16,18 @@ const bcrypt = require('bcrypt');
  * @property {string[]} Actors - An array of actor names who starred in the movie.
  * @property {string} ImagePath - The URL path to an image representing the movie (e.g., a poster).
  * @property {boolean} Featured - A boolean indicating whether the movie is featured.
- */
+ */ 
 
 /**
  * @typedef {Object} Genre
+ * @description Represents a movie genre.
  * @property {string} Name - The name of the genre (e.g., Action, Comedy, etc.).
  * @property {string} Description - A brief description of the genre.
  */
 
 /**
  * @typedef {Object} Director
+ * @description Represents a movie director.
  * @property {string} Name - The name of the director.
  * @property {string} Bio - A brief biography of the director.
  */
@@ -48,15 +50,21 @@ let movieSchema = mongoose.Schema({
 /**
  * @schema User
  * @description Mongoose schema for a User. Contains fields such as username, password, email, birthday, and favorite movies.
- */
-
-/**
- * @typedef {Object} User
  * @property {string} Username - The unique username for the user (must be alphanumeric).
  * @property {string} Password - The user's hashed password.
  * @property {string} Email - The user's email address.
  * @property {Date} Birthday - The user's date of birth.
  * @property {ObjectId[]} FavoriteMovies - An array of movie IDs (ObjectIds) that the user has marked as favorites.
+ */
+
+/**
+ * @typedef {Object} User
+ * @description Represents a user in the application.
+ * @property {string} Username - The username of the user (alphanumeric).
+ * @property {string} Password - The hashed password of the user.
+ * @property {string} Email - The email address of the user.
+ * @property {Date} Birthday - The user's birthdate.
+ * @property {ObjectId[]} FavoriteMovies - A list of movie IDs marked as favorites by the user.
  */
 let userSchema = mongoose.Schema({
     Username: {type: String, required: true},
